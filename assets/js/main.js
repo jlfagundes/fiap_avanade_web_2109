ecd782787d46e58a7138a5a93c3264d903d743c9
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-09-28 11:38:27 -0300

    inicio de funcao e validar form com HTML5

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// trabalhando com JavaScript em um arquivo externo
+
+const nome = "FIAP"
+console.log(nome, 'chamando variavel criada com var');
+
+// escreve("Exemplo de função Nominal (udf)");
+
+function escreve(titulo) {
+    alert(titulo);
+}
