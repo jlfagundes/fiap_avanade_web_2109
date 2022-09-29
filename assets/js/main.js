2ed53ab4916f04fc60c395860c1841c8f96511bf
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-09-29 10:47:34 -0300

    trabalhando com funcao, data e tempo

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,19 @@ console.log(nome, 'chamando variavel criada com var');
 function escreve(titulo) {
     alert(titulo);
 }
+
+// function expression (expressão de função) usando uma função anonima (quando não tem nome)
+const mudaTitulo = function(newText) {
+    document.querySelector('title').innerHTML = newText;
+}
+
+// mudaTitulo('Trocando titulo da página');
+
+
+// usando setInterval
+const relogio  = function() {
+    const data = new Date();
+    document.querySelector('title').innerHTML = data.toLocaleTimeString();
+}
+
+setInterval(relogio, 1000)
