15aa5317622d604e931bcf606189e6d0b4e7356c
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-09-30 10:57:58 -0300

    spread operator and desestructuring assignment

---
+++ assets/js/spread.js
@@ -0,0 +1,29 @@
+/*
+// desestruturando array
+const airNac = ['Azul', 'Gol', 'Latam'];
+
+const airInt = [...airNac, 'Delta', 'United Airlines'];
+
+console.log('valor dentro de airInt => ', airInt);
+
+const [azul, gol, latam, delta, ...internationals] = airInt;
+
+console.log('variaveis extraidas de airInt => ', azul, gol, latam, delta, internationals);
+
+console.log('valor dentro de airInt depois de desestruturar => ', airInt);
+
+
+// desestruturando objeto
+
+const userData = {
+    name: "jose",
+    email: "jlfagundes@gmail.com",
+    password: "123456",
+};
+
+const {password, ...data} = userData;
+
+console.log('dados do objeto userData => ', data)
+*/
+
+// Aprendendo sobre eventos
