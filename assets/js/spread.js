c25044b3bd02eaa074c61a77550f4caa64435a3e
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-09-30 11:31:13 -0300

    entendo eventos no JS

--- assets/js/spread.js
+++ assets/js/spread.js
@@ -27,3 +27,22 @@ console.log('dados do objeto userData => ', data)
 */
 
 // Aprendendo sobre eventos
+//eventos - JavaScript é uma linguagem dirigida a eventos. Algo acontece, então reagimos.
+// Eventos de tempo - setTimeout (espera o tempo e executa 1 vez), setInterval (executa a cada intervalo de tempo)
+// Eventos de DOM - onClick, onChange, onSubmit (manipular o HTML)
+// Eventos de teclado - onKeyDown, onKeyUp, onKeyPress
+// Eventos de mouse - onMouseDown, onMouseUp, onMouseMove
+// Eventos de formulário - onFocus (clica no campo), onBlur (sai do campo), onChange
+// Eventos de mídia - onPlay, onPause, onEnded
+// Eventos de rede - onLoad (quando a pagina carregou, imagem carregou ou não), onError, onAbort
+
+const body = document.querySelector('body');
+body.style.width = '100%';
+body.style.height = '100vh';
+
+body.addEventListener('mouseover', (event) => {
+    body.style.background = '#d4d4d4'
+    console.log('executou o evento')
+});
+
+console.log('teste')
