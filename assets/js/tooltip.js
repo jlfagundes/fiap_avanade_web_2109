8bca833a46a93805b4acec6dae8f9b5de42950cc
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-04 11:55:38 -0300

    desafio função tooltip

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -16,32 +16,39 @@ body.addEventListener('mouseout', () => {
 })
 */
 
+const newTooltip = function (targetElement, message, e) {
+    const newDiv = document.createElement('div');
+    const largura = 200;
+    newDiv.classList.add('tooltip');
+    newDiv.innerText = message;
+    newDiv.style.position = 'absolute';
+    newDiv.style.top = e.clientY + 'px';
+    newDiv.style.width = largura + 'px';
+    newDiv.style.left = (e.clientX + largura > body.clientWidth ? e.clientX - largura - 20 : e.clientX + largura) + 'px';
+    newDiv.style.display = 'flex';
+    console.log(newDiv.style.width);
+    header.appendChild(newDiv);
+}
+
 tooltip.addEventListener('mouseover', (e) => {
-    console.log('Altura da página', body.clientHeight);
-    console.log('Largura da página', body.clientWidth);
-    console.log('Altura do Tootip', tooltip.clientHeight);
-    console.log('Largura do tooltip', tooltip.clientWidth);
-    console.log('Altura do mouse', e.clientY);
-    console.log('Lagura do mouse', e.clientX);
+    // console.log('Altura da página', body.clientHeight);
+    // console.log('Largura da página', body.clientWidth);
+    // console.log('Altura do Tootip', tooltip.clientHeight);
+    // console.log('Largura do tooltip', tooltip.clientWidth);
+    // console.log('Altura do mouse', e.clientY);
+    // console.log('Lagura do mouse', e.clientX);
 
     if (document.querySelector('.tooltip')) {
         document.querySelector('.tooltip').remove();
     }
-    const newDiv = document.createElement('div');
-    const largura = 200;
-    newDiv.classList.add('tooltip');
-    newDiv.innerText = 'Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass.';
-    newDiv.style.position = 'absolute';
-    newDiv.style.top = e.clientY + 'px';
-    const getNewDiv = document.querySelector('.tooltip');
-    newDiv.style.display = 'flex';
-    newDiv.style.width = largura + 'px'
-    newDiv.style.left = (e.clientX + largura > body.clientWidth ? e.clientX - largura - 20 : e.clientX + largura) + 'px';
-    console.log(newDiv.style.width);
-    header.appendChild(newDiv);
+    
+    newTooltip(tooltip, 'Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass.', e);
+
 })
 tooltip.addEventListener('mouseout', () => {
     if (document.querySelector('.tooltip')) {
         document.querySelector('.tooltip').remove();
     }
 })
+
+
