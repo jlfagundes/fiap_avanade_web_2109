e72b23c4623f272e2254db9521b9fedad1c9d31b
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-05 09:45:44 -0300

    fix tooltip function

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,7 +1,7 @@
 // Utilizando tooltip - Eventos com JavaScript
 
 // Selecionando elementos HTML
-const tooltip = document.querySelector('#tooltip');
+// const tooltip = document.querySelector('#tooltip');
 const body = document.querySelector('body');
 const header = document.querySelector('.top-site');
 
@@ -16,39 +16,36 @@ body.addEventListener('mouseout', () => {
 })
 */
 
-function newTooltip(targetElement, message, e) {
-    const newDiv = document.createElement('div');
-    const largura = 200;
-    newDiv.classList.add('tooltip');
-    newDiv.innerText = message;
-    newDiv.style.position = 'absolute';
-    newDiv.style.top = e.clientY + 'px';
-    newDiv.style.width = largura + 'px';
-    newDiv.style.left = (e.clientX + largura > body.clientWidth ? e.clientX - largura - 20 : e.clientX + largura) + 'px';
-    newDiv.style.display = 'flex';
-    console.log(newDiv.style.width);
-    targetElement.appendChild(newDiv);
+const tooltip = function (target, message) {
+    const targetElement = document.querySelector(target);
+    targetElement.addEventListener('mouseover', (e) => {
+        if (document.querySelector('.tooltip')) {
+            document.querySelector('.tooltip').remove();
+        }
+        const newDiv = document.createElement('div');
+        const largura = 200;
+        newDiv.classList.add('tooltip');
+        newDiv.innerText = message;
+        newDiv.style.position = 'absolute';
+        newDiv.style.top = e.clientY + 'px';
+        newDiv.style.width = largura + 'px';
+        newDiv.style.left = (e.clientX + largura > body.clientWidth ? e.clientX - largura - 20 : e.clientX + largura) + 'px';
+        newDiv.style.display = 'flex';
+        targetElement.parentNode.appendChild(newDiv);
+        // console.log('Altura da página', body.clientHeight);
+        // console.log('Largura da página', body.clientWidth);
+        // console.log('Altura do Tootip', tooltip.clientHeight);
+        // console.log('Largura do tooltip', tooltip.clientWidth);
+        // console.log('Altura do mouse', e.clientY);
+        // console.log('Lagura do mouse', e.clientX);
+    })
+    targetElement.addEventListener('mouseout', () => {
+        if (document.querySelector('.tooltip')) {
+            document.querySelector('.tooltip').remove();
+        }
+    })
 }
 
-tooltip.addEventListener('mouseover', (e) => {
-    // console.log('Altura da página', body.clientHeight);
-    // console.log('Largura da página', body.clientWidth);
-    // console.log('Altura do Tootip', tooltip.clientHeight);
-    // console.log('Largura do tooltip', tooltip.clientWidth);
-    // console.log('Altura do mouse', e.clientY);
-    // console.log('Lagura do mouse', e.clientX);
 
-    if (document.querySelector('.tooltip')) {
-        document.querySelector('.tooltip').remove();
-    }
-    
-    newTooltip(header, 'Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass.', e);
-
-})
-tooltip.addEventListener('mouseout', () => {
-    if (document.querySelector('.tooltip')) {
-        document.querySelector('.tooltip').remove();
-    }
-})
-
+tooltip("#tooltip", 'Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass.');
 
